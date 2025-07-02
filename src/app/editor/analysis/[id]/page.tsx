9723e8a1655f5216e5a8cfcbeb10510d28be1df3
yurimeeee <kplhn6@gmail.com>
2025-07-02 22:05:56 +0900

    feat: csv 다운로드 기능 추가

--- src/app/editor/analysis/[id]/page.tsx
+++ src/app/editor/analysis/[id]/page.tsx
@@ -1,29 +1,24 @@
 'use client';
 
-import { CustomDialog, DialogTrigger } from '@components/ui/dialog';
-import { GRAY_500, GRAY_600 } from '@styles/colors';
-import { ReactNode, useEffect, useState } from 'react';
 import { auth, db } from '@lib/firebase';
 import { collection, getDocs, setDoc } from 'firebase/firestore';
-import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
-import { useParams, useSearchParams } from 'next/navigation';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 import { AttendeeInfoData } from '@type/templates';
 import BeatLoader from 'react-spinners/BeatLoader';
-import { BsFillInfoCircleFill } from 'react-icons/bs';
 import { Button } from '@components/ui/button';
-import CustomAccordion from '@components/editor/feature/templates/custom/Accordion';
+import { CSVLink } from 'react-csv';
 import { CustomButton } from '@components/ui/CustomButton';
 import { CustomInfoText } from '@components/ui/CustomInfoText';
 import { CustomTooltip } from '@components/ui/tooltip';
-import KakaoMap from '@components/editor/feature/KakaoMap';
 import SampleGreetingMessageModal from '@components/editor/feature/templates/custom/modal/SampleGreetingMessageModal';
-import ShareSettingsModal from '@components/editor/feature/templates/custom/modal/ShareSettingsModal';
 import dayjs from 'dayjs';
+import { formatUnixTimestamp } from '@utils/func';
 import { motion } from 'framer-motion';
 import styled from '@emotion/styled';
 import theme from '@styles/theme';
 import { toast } from 'sonner';
+import { useParams } from 'next/navigation';
 
 const Title = styled.div`
   border-top-left-radius: 8px;
@@ -42,18 +37,43 @@ const Wrap = styled.div`
   width: 100%;
   /* max-width: 720px; */
 `;
+const TabItem = styled.div<{ active: boolean }>`
+  width: 100%;
+  height: 36px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 16px 12px;
+
+  transition: all.3s;
+  color: ${(props) => {
+    return props.active ? theme.color.textDefault : theme.color.gray_400;
+  }};
+  border-bottom: ${(props) => {
+    return props.active ? `2px solid ${theme.color.textDefault}` : `1px solid ${theme.color.gray_300}`;
+  }};
+`;
+
+const InfoCard = ({ label, value }: { label: string; value?: number }) => (
+  <div className="shadow-default w-full px-6 py-4 bg-white font-suite text-left text-sm font-medium rounded-md">
+    <p className="text-muted-foreground mb-6">{label}</p>
+    <p className="text-text-default text-lg">{value} 명</p>
+  </div>
+);
 
 export default function AnalysisPage() {
   const params = useParams();
   const id = params.id;
-  const searchParams = useSearchParams();
-  const isEdit = searchParams.get('edit') === 'true';
-  const type = searchParams.get('type');
   const [loading, setLoading] = useState<boolean>(true);
   const [sampleGreetingMessageModal, setSampleGreetingMessageModal] = useState<any>({ open: false, title: '', type: '' });
   const [attendeesList, setAttendeesList] = useState<AttendeeInfoData[] | null>(null);
-  const [attending, setAttending] = useState<AttendeeInfoData[] | null>(null);
-  const [declined, setDeclined] = useState<AttendeeInfoData[] | null>(null);
+  const [excelData, setExcelData] = useState<any>([]);
+  const csvLink = useRef<any>(null);
+  const [activeTab, setActiveTab] = useState<string>('groom');
+  const tabList: { text: string; value: string }[] = [
+    { text: '신랑측', value: 'groom' },
+    { text: '신부측', value: 'bride' },
+  ];
 
   const fetchAttendeesList = async () => {
     setLoading(true);
@@ -74,17 +94,59 @@ export default function AnalysisPage() {
   useEffect(() => {
     fetchAttendeesList();
   }, []);
-  useEffect(() => {
-    if (attendeesList && attendeesList?.length > 0) {
-      const attendeeArr = attendeesList?.filter((item: AttendeeInfoData) => item?.attendance === true);
-      setAttending(attendeeArr);
-      const declinedArr = attendeesList?.filter((item: AttendeeInfoData) => item?.attendance === false);
-      setDeclined(declinedArr);
-    }
-  }, [attendeesList]);
+
   console.log('attendeesList', attendeesList);
-  console.log('attending', attending);
-  console.log('declined', declined);
+
+  const attending = useMemo(() => attendeesList?.filter((item) => item.attendance === true), [attendeesList]);
+  const declined = useMemo(() => attendeesList?.filter((item) => item.attendance === false), [attendeesList]);
+
+  const guestsByTab = useMemo(() => {
+    return attendeesList?.filter((item) => item.whose_guest === activeTab);
+  }, [attendeesList, activeTab]);
+
+  const attendingByTab = guestsByTab?.filter((item) => item.attendance === true).length;
+  const declinedByTab = guestsByTab?.filter((item) => item.attendance === false).length;
+
+  const headers = [
+    { label: '구분', key: 'whose_guest' },
+    { label: '참석여부', key: 'attendance' },
+    { label: '식사여부', key: 'eat_or_not' },
+    { label: '성함', key: 'name' },
+    { label: '동행인원', key: 'number_of_accompany' },
+    { label: '메시지', key: 'memo' },
+    { label: '제출일시', key: 'createdAt' },
+  ];
+
+  const handleDownloadCSV = () => {
+    const tmp: any[] =
+      attendeesList?.map((item: any) => {
+        return {
+          whose_guest: item.whose_guest === 'groom' ? '신랑측' : '신부측',
+          attendance: item.attendance ? 'Y' : 'N',
+          eat_or_not: item.eat_or_not ? 'Y' : 'N',
+          name: item.name,
+          number_of_accompany: item.number_of_accompany,
+          memo: item.memo,
+          createdAt: formatUnixTimestamp(item?.createdAt?.seconds, item?.createdAt?.nanoseconds),
+          createdAtSort: item?.createdAt?.seconds ?? 0, // 정렬용 필드
+        };
+      }) ?? [];
+
+    // createdAt 기준 오름차순 정렬
+    const sorted = tmp.sort((a, b) => a.createdAtSort - b.createdAtSort);
+
+    // 정렬용 필드 삭제
+    const finalData = sorted.map(({ createdAtSort, ...rest }) => rest);
+
+    setExcelData(finalData);
+  };
+
+  useEffect(() => {
+    if (excelData?.length > 0) {
+      csvLink?.current?.link.click();
+      setExcelData([]);
+    }
+  }, [excelData]);
   return (
     <div className="flex h-screen">
       {loading ? (
@@ -100,11 +162,12 @@ export default function AnalysisPage() {
                 <p className="text-muted-foreground">참석 의사 응답</p>
               </Title>
               <Content>
+                <p className="text-[18px] font-suite-bold text-gray-600 mb-6">전체 요약</p>
                 <div className="shadow-default w-full px-6 py-4 bg-white font-suite text-left text-sm font-medium rounded-md">
                   <p className="text-muted-foreground mb-6">총 응답 수</p>
                   <p className="text-text-default text-lg">{attendeesList?.length} 명</p>
                 </div>
-                <div className="flex gap-4 mt-4">
+                <div className="flex gap-4 mt-4 mb-6">
                   <div className="shadow-default w-full px-6 py-4 bg-white font-suite text-left text-sm font-medium rounded-md">
                     <p className="text-muted-foreground mb-6">참석</p>
                     <p className="text-text-default text-lg">{attending?.length} 명</p>
@@ -114,19 +177,29 @@ export default function AnalysisPage() {
                     <p className="text-text-default text-lg">{declined?.length} 명</p>
                   </div>
                 </div>
+                <Button text="엑셀 다운로드" variant="pink" onClick={handleDownloadCSV} className="mb-10" />
+                <CSVLink data={excelData} headers={headers} filename="참석여부.csv" className="hidden" ref={csvLink} target="_blank" />
+                <p className="text-[18px] font-suite-bold text-gray-600 mb-6">상세 정보</p>
+
+                <div className="flex">
+                  {tabList?.map((item, idx) => (
+                    <TabItem key={idx} active={item.value === activeTab} onClick={() => setActiveTab(item.value)}>
+                      {item.text}
+                    </TabItem>
+                  ))}
+                </div>
+
+                <div data-aos="fade-up" data-aos-duration="1000">
+                  <div className="flex gap-4 mt-4 mb-6">
+                    <InfoCard label="참석" value={attendingByTab} />
+                    <InfoCard label="불참" value={declinedByTab} />
+                  </div>
+                </div>
               </Content>
             </div>
           </div>
         </Wrap>
       )}
-
-      {/* <SampleGreetingMessageModal
-        open={sampleGreetingMessageModal.open}
-        onOpenChange={setSampleGreetingMessageModal}
-        title={'샘플 문구 활용'}
-        setData={(newData: any) => setFormData({ ...formData, ...newData })}
-        data={formData}
-      /> */}
     </div>
   );
 }
